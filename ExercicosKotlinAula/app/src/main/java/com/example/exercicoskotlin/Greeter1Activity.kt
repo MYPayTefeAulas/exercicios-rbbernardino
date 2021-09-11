09d359d6d2682ec943bb5cff307ef6a2a8f0433c
Rodrigo Bernardino <rbbernardino@gmail.com>
2021-09-11 10:54:27 -0300

    adiciona comentários ao código do greeter

--- ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/Greeter1Activity.kt
+++ ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/Greeter1Activity.kt
@@ -5,9 +5,20 @@ import android.os.Bundle
 import com.example.exercicoskotlin.databinding.ActivityGreeter1Binding
 
 class Greeter1Activity : AppCompatActivity() {
+    // binding que permite manipular elementos da tela
     private lateinit var binding: ActivityGreeter1Binding
+
+    // lista de String "mutável", inicializada vazia
     private val listaNomes = mutableListOf<String>()
+
+    // índice inicia em zero, a cada impressão aumenta
+    // chegou no tamanho da lista reinicia
     private var indiceAtual: Int = 0
+
+    // mantém objeto GreeterTipo1
+    // botão "trocar" alterna entre as várias possibilidades
+    // lateinit permite que eu inicialize o objeto depois, observe que "indiceAtual", acima, já
+    // recebe um valor inicial (zero), veja o igual (=), símbolo de atribuição
     private lateinit var greeterAtual: GreeterTipo1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,19 +32,24 @@ class Greeter1Activity : AppCompatActivity() {
         val greeter2 = GreeterTipo1("Bem Vindo,")
 
         // inicialização do greeterAtual
-        greeterAtual = greeter1 // lateinit confia que farei isso
+        // lateinit confia que farei isso antes de usar a primeira vez
+        greeterAtual = greeter1
 
+        // preenche lista com nomes de pessoas
         listaNomes.add("Rodrigo")
         listaNomes.add("Alex")
         listaNomes.add("Robert")
 
         // --------------------------------------------------
         // configuração dos botões
-        binding.btImprimir.setOnClickListener {
-            val nomeAtual = listaNomes[indiceAtual]
 
+        // BOTÃO IMPRIMIR PRÓXIMO
+        binding.btImprimir.setOnClickListener {
+            // preenche TextView com a string na posição da lista "indiceAtual"
+            val nomeAtual = listaNomes[indiceAtual]
             binding.txtSaida.text = greeterAtual.greet(nomeAtual)
 
+            // lógica para fazer nomes circularem, reiniciando contador de "indiceAtual"
             if(indiceAtual == listaNomes.size - 1) {
                 indiceAtual = 0
             } else {
@@ -41,7 +57,9 @@ class Greeter1Activity : AppCompatActivity() {
             }
         }
 
+        // BOTÃO TROCAR GREETER
         binding.btTrocar.setOnClickListener {
+            // usei apenas dois greeters, mas poderia usar vários, com uma lista, por exemplo
             if(greeterAtual == greeter1) {
                 greeterAtual = greeter2
                 binding.txtNumGreeter.text = "2"
@@ -50,7 +68,6 @@ class Greeter1Activity : AppCompatActivity() {
                 binding.txtNumGreeter.text = "1"
             }
         }
-
         setContentView(binding.root)
     }
 }
--- ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/GreeterTipo1.kt
+++ ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/GreeterTipo1.kt
@@ -1,7 +1,13 @@
 package com.example.exercicoskotlin
 
+// declaro o atributo enquanto crio construtor nos parênteses
 class GreeterTipo1(val cumprimento: String) {
 
+    // método greet que estará presente em todos os greeters
+    // lembre do carro: o acelerador é igual para todos os carros, mas o motor e todo o resto não
+    //
+    // o conteúdo de "cumprimento" varia entre os diferentes greeters, mas a lógica de juntar
+    // cumprimento + nome não
     fun greet(nome: String): String {
         val cumprimentoCompleto = "$cumprimento $nome"
         return cumprimentoCompleto
