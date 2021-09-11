3c57e5fd51565fcfd968efac6ed8fd06c419f88a
Rodrigo Bernardino <rbbernardino@gmail.com>
2021-09-11 09:32:22 -0300

    finaliza exemplo 1 dado em aula

--- ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/Greeter1Activity.kt
+++ ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/Greeter1Activity.kt
@@ -2,10 +2,55 @@ package com.example.exercicoskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exercicoskotlin.databinding.ActivityGreeter1Binding
 
 class Greeter1Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityGreeter1Binding
+    private val listaNomes = mutableListOf<String>()
+    private var indiceAtual: Int = 0
+    private lateinit var greeterAtual: GreeterTipo1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_greeter1)
+
+        binding = ActivityGreeter1Binding.inflate(layoutInflater)
+
+        // --------------------------------------------------
+        // configuração de variáveis "globais" da tela
+        val greeter1 = GreeterTipo1("Olá,")
+        val greeter2 = GreeterTipo1("Bem Vindo,")
+
+        // inicialização do greeterAtual
+        greeterAtual = greeter1 // lateinit confia que farei isso
+
+        listaNomes.add("Rodrigo")
+        listaNomes.add("Alex")
+        listaNomes.add("Robert")
+
+        // --------------------------------------------------
+        // configuração dos botões
+        binding.btImprimir.setOnClickListener {
+            val nomeAtual = listaNomes[indiceAtual]
+
+            binding.txtSaida.text = greeterAtual.greet(nomeAtual)
+
+            if(indiceAtual == listaNomes.size - 1) {
+                indiceAtual = 0
+            } else {
+                indiceAtual++
+            }
+        }
+
+        binding.btTrocar.setOnClickListener {
+            if(greeterAtual == greeter1) {
+                greeterAtual = greeter2
+                binding.txtNumGreeter.text = "2"
+            } else {
+                greeterAtual = greeter1
+                binding.txtNumGreeter.text = "1"
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
