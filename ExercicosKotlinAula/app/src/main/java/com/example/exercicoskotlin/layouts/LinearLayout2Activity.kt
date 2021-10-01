97bcb122f6342ff5e3a7f4086267816a5a7c8798
Rodrigo Bernardino <rbbernardino@gmail.com>
2021-10-01 15:10:07 -0300

    implementa activity outros componentes

---
+++ ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/layouts/LinearLayout2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.exercicoskotlin.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayout2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout2)
+    }
+}
---
+++ ExercicosKotlinAula/app/src/main/java/com/example/exercicoskotlin/layouts/OutrosComponentesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.exercicoskotlin.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutrosComponentesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_outros_componentes)
+    }
+}
